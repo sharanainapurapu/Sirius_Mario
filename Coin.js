dd59db40ed131ace78c0594800a74200303b7b85
Sharan Ainapurapu <sharanainapurapu@gmail.com>
2019-04-12 10:36:32 +0530

    Update Coin.js

--- Coin.js
+++ Coin.js
@@ -1,23 +1,23 @@
-function coin(i,j)						//Konstruktor einer Münze
+function coin(i,j)						
 {
-	this.i = i;							//damit ist die Münze eindeutig identifizierbar
+	this.i = i;							//damit ist die MÃ¼nze eindeutig identifizierbar
 	this.j = j;
-	this.x = 32*i;						//world-Koordinaten -> für Animation in GameLoop()
+	this.x = 32*i;						//world-Koordinaten -> fÃ¼r Animation in GameLoop()
 	this.y = (14-j)*32;
 	
-	$('#world').append('<div id=\'coin' + i.toString() + '_' + j.toString() + '\'></div>');		//erzeuge div für die Münze und setze sie in die Spielwelt ein
-	this.body = $('#coin'+i.toString()+'_'+j.toString());										//hiermit lässt sich das Münzen-div ansprechen
+	$('#world').append('<div id=\'coin' + i.toString() + '_' + j.toString() + '\'></div>');		//erzeuge div fÃ¼r die MÃ¼nze und setze sie in die Spielwelt ein
+	this.body = $('#coin'+i.toString()+'_'+j.toString());										//hiermit lÃ¤sst sich das MÃ¼nzen-div ansprechen
 	
 	this.body.css( { 'margin' : '0' ,
 					 'padding' : '0' ,
 		 		 	 'width' : '32px' , 
 		 		 	 'height' : '32px' , 
 		 			 'background-image' : 'url(\'mario-objects.png\')' , 
-					 'background-position' : '0px 0px' ,	 	//Bild der Münze
-					 'position' : 'absolute' ,					//Positionierung der Münze erfolgt relativ zum übergeordneten world-Div
+					 'background-position' : '0px 0px' ,	 	//Bild der MÃ¼nze
+					 'position' : 'absolute' ,					//Positionierung der MÃ¼nze erfolgt relativ zum Ã¼bergeordneten world-Div
 					 'left' : (32*i).toString() + 'px' ,
 					 'bottom' : ((14-j)*32).toString() + 'px' ,
-					 'z-index' : '10'  } );						//Münzen sind hinter Mario
+					 'z-index' : '10'  } );						//MÃ¼nzen sind hinter Mario
 	
-	this.body.sprite( { fps: 10 , no_of_frames: 4 } );			//damit sich die Münze dreht
+	this.body.sprite( { fps: 10 , no_of_frames: 4 } );			//damit sich die MÃ¼nze dreht
 };
